d797ab6efdb638aaad48acfce77f8f41a0e2cf67
zzzxxxxxx <v5qibaba@163.com>
2019-06-25 17:10:20 +0800

    initialization of project style

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,8 @@ module.exports = {
   ** Global CSS
   */
   css: [
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/index.css',
+    '@/assets/css/reset.min.css'
   ],
   /*
   ** Plugins to load before mounting the App
