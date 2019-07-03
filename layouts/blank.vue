b543dae3d24188c975e0ee0bcde1b250a12d18ce
zzzxxxxxx <v5qibaba@163.com>
2019-07-03 10:31:28 +0800

    login page counstruction success

---
+++ layouts/blank.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="layouts-blank">
+    <nuxt />
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data: () => ({
+
+  })
+}
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="page-login">
+    <div class="login-panel">
+      <div class="banner">
+        <img src="https://s0.meituan.net/bs/file/?f=fe-sso-fs:build/page/static/banner/www.jpg" alt="">
+      </div>
+      <div class="form">
+        <h4>管理员登录</h4>
+        <el-input class="username" v-model="input" placeholder="用户名"></el-input>
+        <el-input class="password" v-model="pwd" placeholder="密码" show-password></el-input>
+        <p>
+          <nuxt-link to="">忘了密码？？</nuxt-link>
+        </p>
+        <el-button class="form-btn">登录</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  layout: 'blank',
+  data: () => ({
+    input: '',
+    pwd: ''
+  })
+}
+</script>
+<style lang="scss">
+@import '@/assets/css/login/index.scss'
+</style>
